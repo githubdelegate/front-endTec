3613f8e337d96c115ee27d55e19f035147ea2869
githubdelegate <@ostmail@163.com>
2019-03-06 13:53:55 +0800

    0306-01

--- src/pages/input/index.vue
+++ src/pages/input/index.vue
@@ -24,7 +24,19 @@
           <div class="weui-cell__bd">添加更多</div>
         </div>
       </div>
-      <div appear-active-class=""></div>
+      <div class="weui-cells__title">复选</div>
+      <div class="weui-cells weui-cells_after-title">
+        <checkbox-group @change="checkboxChange">
+          <label class="weui-cell weui-check__label" v-for="(item,index) in checkboxItems" :key="index">
+            <checkbox class="weui-check" :value="item.value" :checked="item.checked" />
+            <div class="weui-cell__hd weui-cell__hd_in-checkbox">
+              <icon class="weui-icon-checkbox_circle" type="circle" size="23" v-if="!item.checked"></icon>
+              <icon class="weui-icon-checkbox_success" type="success" size="23" v-if="item.checked"></icon>
+            </div>
+            <div class="weui-cell__bd">{{item.name}}</div>
+          </label>
+        </checkbox-group>
+      </div>
     </div>
 
   </div>
@@ -47,7 +59,7 @@ export default {
         }
       ],
       checkboxItems: [
-        {name: 'detail', value: '0', checked: true},
+        {name: 'detail', value: '0', checked: false},
         {name: 'detail', value: '1', checked: true},
       ],
       time: '09:01',
@@ -63,9 +75,25 @@ export default {
     radioChange(e){
       let radioItems = this.radioItems;
       for (let i= 0; i < radioItems.length;i++){
-        radioItems[i].checked = radioItems[i].value == e.mp.detail.value;
+        radioItems[i].checked = radioItems[i].value === e.mp.detail.value;
       }
       this.radioItems = radioItems;
+    },
+    checkboxChange(e) {
+      console.log('checkbox发生change事件，携带value值为：' + e.mp.detail.value);
+      var checkboxItems = this.checkboxItems;
+      var values = e.mp.detail.value;
+      for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+        checkboxItems[i].checked = false;
+
+        for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
+          if (checkboxItems[i].value === values[j]) {
+            checkboxItems[i].checked = true;
+            break;
+          }
+        }
+      }
+      this.checkboxItems = checkboxItems;
     }
   },
 }
